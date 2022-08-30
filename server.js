e948b40f9148a2eaef301f0bbc7e368672ed1994
adnanGujjar <adnan.yaqob.gujar@gmail.com>
2022-08-30 11:46:19 +0500

    Server Pushed

---
+++ server.js
@@ -0,0 +1,24 @@
+const express= require("express")
+const cors= require("cors")
+const app= express()
+const setupDB = require("./src/config/db");
+const authRoutes = require("./src/routes/authRoutes")
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+setupDB()
+
+app.use("/auth", authRoutes)
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my application." });
+});
+
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
